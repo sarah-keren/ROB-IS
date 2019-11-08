c829e371045ddff0c4092f9d295f8976e6bac363
Gerard Canal <grar.knal@gmail.com>
2019-11-08 10:53:40 +0000

    Uniform bananas and doughnuts

--- rosplan_waypoint_sampling/src/HiddenCostPublisher.py
+++ rosplan_waypoint_sampling/src/HiddenCostPublisher.py
@@ -28,10 +28,14 @@ class HiddenCostMap:
         prob = math.e ** (-((d - mu) ** 2) / (2 * sigma ** 2)) / math.sqrt(2 * math.pi * sigma ** 2)
         return prob
 
+    def _uniform_doughnut(selfself, p, c, mu, sigma):
+        d = math.sqrt((p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2)
+        return 100*(d > mu-2*sigma and d < mu+2*sigma)
+
     def _banana(self, p, c, angle, arclen, mu, sigma):
         gamma = angle - arclen/2.0
         theta = gamma + arclen
-        k = math.atan2(c[0]-p[0], c[1]-p[1])
+        k = math.atan2(c[0]-p[0], c[1]-p[1])  # angle of point p
         #if k >= gamma and k <= theta:
         if k < gamma:
             probangle = math.e ** (-((k - gamma) ** 2) / (2 * sigma ** 2)) / math.sqrt(2 * math.pi * sigma ** 2)
@@ -42,6 +46,49 @@ class HiddenCostMap:
         d = math.sqrt((p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2)
         prob = math.e ** (-((d - mu) ** 2) / (2 * sigma ** 2)) / math.sqrt(2 * math.pi * sigma ** 2)
         return prob*probangle
+
+    def _first_quadrant(self, a):
+        if a < math.pi/2:
+            return a
+        elif a < math.pi:
+            return math.pi - a
+        elif a < 3*math.pi/2:
+            return math.pi + x
+        return 2*math.pi-a
+
+    def _uniform_banana(self, p, c, angle, arclen, mu, sigma):
+        gamma = (angle+arclen / 2.0)
+        theta = -(gamma)
+        k = math.atan2(c[0] - p[0], c[1] - p[1])  # angle of point p
+
+        dist = 2*sigma
+
+        #gp = (c[0] - mu * math.cos(gamma-angle), c[1] - mu * math.sin(gamma-angle))
+        #tp = (c[0] + mu * math.cos(theta-angle), c[1] + mu * math.sin(theta-angle))
+
+        #gp = (c[0] + mu * math.sin(gamma-angle), c[1] + mu * math.cos(gamma-angle))
+        #tp = (c[0] + mu * math.sin(theta-angle), c[1] + mu * math.cos(theta-angle))
+
+        # gp = (c[0] - abs(mu * math.sin(gamma - angle)), c[1] - abs(mu * math.cos(gamma - angle)))
+        # tp = (c[0] - abs(mu * math.sin(theta + angle)), c[1] + abs(mu * math.cos(theta + angle)))
+        #
+        # gp = (c[0] - (mu * math.sin(gamma)), c[1] - (mu * math.cos(gamma))) # correct 0 angle
+        # tp = (c[0] - (mu * math.sin(theta)), c[1] - (mu * math.cos(theta))) # correct 0 angle
+        #
+        # gp = ((mu * math.sin(gamma)), (mu * math.cos(gamma)))
+        # tp = ((mu * math.sin(theta)), -(mu * math.cos(theta)))
+        # tp = (c[0] - (tp[0] * math.cos(-angle) + tp[1]*math.sin(-angle)), c[1] - (tp[0]*math.sin(-angle) - tp[1]*math.cos(-angle)))
+        # gp = (c[0] - (gp[0] * math.cos(-angle) + gp[1]*math.sin(-angle)), c[1] - (gp[0]*math.sin(-angle) - gp[1]*math.cos(-angle)))
+
+        if k <= gamma and k >= theta:
+            return self._uniform_doughnut(p, c, mu, sigma)
+        # d = math.sqrt((p[0] - gp[0]) ** 2 + (p[1] - gp[1]) ** 2)
+        # r = d <= dist
+        # if not r:
+        #     d = math.sqrt((p[0] - tp[0]) ** 2 + (p[1] - tp[1]) ** 2)
+        #     r = d <= dist
+        # return 100*r
+        return 0
 
     def gen_costmap(self):
 
@@ -73,12 +120,18 @@ class HiddenCostMap:
                     #for (posx, posy, r, std_dev) in self.doughnuts:
                     for elem in self.doughnuts:
                         posx = elem['x']; posy = elem['y']; r = elem['radius']; std_dev = elem['std_dev']
-                        cost += self._doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev) / len(self.doughnuts)
+                        #cost += self._doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev) / len(self.doughnuts)
+                        cost += self._uniform_doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev)
+
+                        #fd = self._uniform_doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev)
+                        #pd = self._doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev)
+                        #assert (pd> 0.01 and  fd > 0) or (fd == 0 and pd < 0.01)
                     #for (posx, posy, r, a, al, std_dev) in self.bananas:
                     for elem in self.bananas:
                         posx = elem['x']; posy = elem['y']; r = elem['radius']; a = elem['angle']; al = elem['arclen']
                         std_dev = elem['std_dev']
-                        cost += self._banana((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), a, al, r, std_dev) / (len(self.bananas) + len(self.doughnuts))
+                        #cost += self._banana((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), a, al, r, std_dev) / (len(self.bananas) + len(self.doughnuts))
+                        cost += self._uniform_banana((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), a, al, r, std_dev)
                     if cost > maxc:
                         maxc = cost
                     grid.data.append(cost)
