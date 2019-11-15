35ef69cd34f3dcf6b3bd411fff4f6b7a06ad2d0d
Gerard Canal <grar.knal@gmail.com>
2019-11-15 19:27:14 +0000

    Merged map publishing

--- rosplan_waypoint_sampling/src/HiddenCostPublisher.py
+++ rosplan_waypoint_sampling/src/HiddenCostPublisher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-import math  
+import math
 from math import floor
 from itertools import product
 from nav_msgs.msg import OccupancyGrid
@@ -10,7 +10,9 @@ class HiddenCostMap:
     def __init__(self):
         self._static_map = None
         self._hidden_costmap = None
-        self.costmap_pub = rospy.Publisher('costmap', OccupancyGrid, queue_size=10)
+        self.costmap_pub = rospy.Publisher('costmap', OccupancyGrid, queue_size=10, latch=True)
+        self.hppits_pub = rospy.Publisher('hppits_map', OccupancyGrid, queue_size=10, latch=True)
+        self.mergemap_pub = rospy.Publisher('merged_map', OccupancyGrid, queue_size=10, latch=True)
 
         if rospy.has_param('~peaks'):
             self.peaks = rospy.get_param('~peaks')
@@ -18,9 +20,9 @@ class HiddenCostMap:
             self.doughnuts = rospy.get_param('~doughnuts')
         if rospy.has_param('~bananas'):
             self.bananas = rospy.get_param('~bananas')
-        self.hppits= []
-        if rospy.has_param('~hppit'):
-            self.hppits = rospy.get_param('~hppit')
+        self.hppits = []
+        if rospy.has_param('~hppits'):
+            self.hppits = rospy.get_param('~hppits')
 
         rospy.Subscriber("map", OccupancyGrid, self.set_map)
 
@@ -28,9 +30,9 @@ class HiddenCostMap:
         self._static_map = msg
 
     def _static_map_cell(self, x, y):
-        gx = int((x - self._static_map.info.origin.position.x)/self._static_map.info.resolution)
-        gy = int((y - self._static_map.info.origin.position.y)/self._static_map.info.resolution)
-        return self._static_map.data[gx + gy*self._static_map.info.width]
+        gx = int((x - self._static_map.info.origin.position.x) / self._static_map.info.resolution)
+        gy = int((y - self._static_map.info.origin.position.y) / self._static_map.info.resolution)
+        return self._static_map.data[gx + gy * self._static_map.info.width]
 
     def _sign(self, n):
         return (n > 0) - (n < 0)
@@ -41,11 +43,12 @@ class HiddenCostMap:
         (xA, yA) = (p[0], p[1])
         (xB, yB) = (c[0], c[1])
         (dx, dy) = (xB - xA, yB - yA)
-        sx = self._static_map.info.resolution * dx/(abs(dx)+abs(dy))
-        sy = self._static_map.info.resolution * dy/(abs(dx)+abs(dy))
+        sx = self._static_map.info.resolution * dx / (abs(dx) + abs(dy))
+        sy = self._static_map.info.resolution * dy / (abs(dx) + abs(dy))
 
-        while (sx == 0 or xA*self._sign(sx) < xB*self._sign(sx)) and (sy == 0 or yA*self._sign(sy) < yB*self._sign(sy)):
-            if self._static_map_cell(xA,yA) > 0.12:
+        while (sx == 0 or xA * self._sign(sx) < xB * self._sign(sx)) and (
+                sy == 0 or yA * self._sign(sy) < yB * self._sign(sy)):
+            if self._static_map_cell(xA, yA) > 0.12:
                 return True
             xA += sx
             yA += sy
@@ -58,21 +61,22 @@ class HiddenCostMap:
 
     def _uniform_doughnut(self, p, c, mu, sigma):
         d = math.sqrt((p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2)
-        if (d > mu-2*sigma and d < mu+2*sigma) and not self._checkCollision(p,c):
-                return 100
+        if (d > mu - 2 * sigma and d < mu + 2 * sigma) and not self._checkCollision(p, c):
+            return 100
         return 0
-   
+
     def _uniform_hppit(self, p, c, mu, sigma):
         d = math.sqrt((p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2)
-        if (d > mu-2*sigma and d < mu+2*sigma) and not self._checkCollision(p,c):
-                return 0
+        #if (d > mu - 2 * sigma and d < mu + 2 * sigma) and not self._checkCollision(p, c):
+        if (d > mu - 2 * sigma and d < mu + 2 * sigma):
+            return 0
         return 100
 
     def _banana(self, p, c, angle, arclen, mu, sigma):
-        gamma = angle - arclen/2.0
+        gamma = angle - arclen / 2.0
         theta = gamma + arclen
-        k = math.atan2(c[0]-p[0], c[1]-p[1])  # angle of point p
-        #if k >= gamma and k <= theta:
+        k = math.atan2(c[0] - p[0], c[1] - p[1])  # angle of point p
+        # if k >= gamma and k <= theta:
         if k < gamma:
             probangle = math.e ** (-((k - gamma) ** 2) / (2 * sigma ** 2)) / math.sqrt(2 * math.pi * sigma ** 2)
         elif k > theta:
@@ -81,29 +85,29 @@ class HiddenCostMap:
             probangle = 1 / math.sqrt(2 * math.pi * sigma ** 2)
         d = math.sqrt((p[0] - c[0]) ** 2 + (p[1] - c[1]) ** 2)
         prob = math.e ** (-((d - mu) ** 2) / (2 * sigma ** 2)) / math.sqrt(2 * math.pi * sigma ** 2)
-        return prob*probangle
+        return prob * probangle
 
     def _first_quadrant(self, a):
-        if a < math.pi/2:
+        if a < math.pi / 2:
             return a
         elif a < math.pi:
             return math.pi - a
-        elif a < 3*math.pi/2:
+        elif a < 3 * math.pi / 2:
             return math.pi + x
-        return 2*math.pi-a
+        return 2 * math.pi - a
 
     def _uniform_banana(self, p, c, angle, arclen, mu, sigma):
-        gamma = (angle+arclen / 2.0)
+        gamma = (angle + arclen / 2.0)
         theta = -(gamma)
         k = math.atan2(c[0] - p[0], c[1] - p[1])  # angle of point p
 
-        dist = 2*sigma
+        dist = 2 * sigma
 
-        #gp = (c[0] - mu * math.cos(gamma-angle), c[1] - mu * math.sin(gamma-angle))
-        #tp = (c[0] + mu * math.cos(theta-angle), c[1] + mu * math.sin(theta-angle))
+        # gp = (c[0] - mu * math.cos(gamma-angle), c[1] - mu * math.sin(gamma-angle))
+        # tp = (c[0] + mu * math.cos(theta-angle), c[1] + mu * math.sin(theta-angle))
 
-        #gp = (c[0] + mu * math.sin(gamma-angle), c[1] + mu * math.cos(gamma-angle))
-        #tp = (c[0] + mu * math.sin(theta-angle), c[1] + mu * math.cos(theta-angle))
+        # gp = (c[0] + mu * math.sin(gamma-angle), c[1] + mu * math.cos(gamma-angle))
+        # tp = (c[0] + mu * math.sin(theta-angle), c[1] + mu * math.cos(theta-angle))
 
         # gp = (c[0] - abs(mu * math.sin(gamma - angle)), c[1] - abs(mu * math.cos(gamma - angle)))
         # tp = (c[0] - abs(mu * math.sin(theta + angle)), c[1] + abs(mu * math.cos(theta + angle)))
@@ -145,42 +149,78 @@ class HiddenCostMap:
 
         rate = rospy.Rate(0.5)
         while not rospy.is_shutdown():
-            grid = OccupancyGrid()
-            grid.header.frame_id = "map"
-            grid.info = self._static_map.info
+            object_grid = OccupancyGrid()
+            object_grid.header.frame_id = "map"
+            object_grid.info = self._static_map.info
+            hppit_grid = OccupancyGrid()
+            hppit_grid.header.frame_id = "map"
+            hppit_grid.info = self._static_map.info
 
-            maxc = 1
-            for y in range(grid.info.height):
-                for x in range(grid.info.width):
-                    cost = 0
+            omaxc = 1
+            hmaxc = 1
+            for y in range(object_grid.info.height):
+                for x in range(object_grid.info.width):
+                    obj_cost = 0
+                    hppit_cost = 0
                     for elem in self.doughnuts:
-                        posx = elem['x']; posy = elem['y']; r = elem['radius']; std_dev = elem['std_dev']
-                        cost += self._uniform_doughnut((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev)
+                        posx = elem['x'];
+                        posy = elem['y'];
+                        r = elem['radius'];
+                        std_dev = elem['std_dev']
+                        obj_cost += self._uniform_doughnut((x * object_grid.info.resolution, y * object_grid.info.resolution),
+                                                       (posx, posy), r, std_dev)
                     for elem in self.bananas:
-                        posx = elem['x']; posy = elem['y']; r = elem['radius']; a = elem['angle']; al = elem['arclen']; std_dev = elem['std_dev']
-                        cost += self._uniform_banana((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), a, al, r, std_dev)
+                        posx = elem['x'];
+                        posy = elem['y'];
+                        r = elem['radius'];
+                        a = elem['angle'];
+                        al = elem['arclen'];
+                        std_dev = elem['std_dev']
+                        obj_cost += self._uniform_banana((x * object_grid.info.resolution, y * object_grid.info.resolution), (posx, posy),
+                                                     a, al, r, std_dev)
                     for elem in self.hppits:
-                        posx = elem['x']; posy = elem['y']; r = elem['radius']; std_dev = elem['std_dev']
-                        cost += self._uniform_hppit((x*grid.info.resolution, y*grid.info.resolution), (posx, posy), r, std_dev)
+                        posx = elem['x'];
+                        posy = elem['y'];
+                        r = elem['radius'];
+                        std_dev = elem['std_dev']
+                        hppit_cost += self._uniform_hppit((x * object_grid.info.resolution, y * object_grid.info.resolution), (posx, posy),
+                                                    r, std_dev)
 
-                    if cost > maxc:
-                        maxc = cost
-                    grid.data.append(cost)
+                    if obj_cost > omaxc:
+                        omaxc = obj_cost
 
-            grid.data = map(lambda c: int(100.0*c/float(maxc)), grid.data)
-		
+                    if hppit_cost > hmaxc:
+                        hmaxc = hppit_cost
+                    object_grid.data.append(obj_cost)
+                    hppit_grid.data.append(hppit_cost)
+
+            object_grid.data = map(lambda c: int(100.0 * c / float(omaxc)), object_grid.data)
+            hppit_grid.data = map(lambda c: int((c == hmaxc) * 100.0 * c / float(hmaxc)), hppit_grid.data)
+
             if preferences:
-                grid = self.add_preferences(grid)
-	    
-			
-            self.costmap_pub.publish(grid)
+                object_grid = self.add_preferences(object_grid)
+
+            self.costmap_pub.publish(object_grid)
+            self.hppits_pub.publish(hppit_grid)
+            self.mergemaps(object_grid, hppit_grid)
             rate.sleep()
 
-    
-    def add_preferences(self,grid):
-	preference_grid = copy.deepcopy(grid)
-	
-	return preference_grid
+    def add_preferences(self, grid):
+        preference_grid = copy.deepcopy(grid)
+
+        return preference_grid
+
+
+    def mergemaps(self, object_map, hiddenpref_map):
+        merged = OccupancyGrid()
+        merged.header.frame_id = "map"
+        merged.info = self._static_map.info
+        for y in range(object_map.info.height):
+            for x in range(object_map.info.width):
+                v = (int(object_map.data[y*object_map.info.width + x]) * int(hiddenpref_map.data[y*hiddenpref_map.info.width + x]))
+                v = v/100
+                merged.data.append(v)
+        self.mergemap_pub.publish(merged)
 
 
 if __name__ == '__main__':
--- rosplan_waypoint_sampling/src/RPRoadmapFilter.cpp
+++ rosplan_waypoint_sampling/src/RPRoadmapFilter.cpp
@@ -30,12 +30,14 @@ namespace KCL_rosplan {
         nh_.param<std::string>("wp_reference_frame", wp_reference_frame_, "map");
         nh_.param<std::string>("rosplan_kb_name", rosplan_kb_name, "rosplan_knowledge_base");
         nh_.param<std::string>("costmap_topic", costmap_topic_, "costmap_topic");
+        nh_.param<std::string>("hppits_topic", hppits_topic_, "hppits_map");
         nh_.param<double>("minimum_sample_separation", minimum_radius_, 1.0);
         nh_.param<bool>("animate_sampling", animate_sampling_, false);
 
 
         // subscriptions of this node, robot odometry and costmap
         map_sub_ = nh_.subscribe<nav_msgs::OccupancyGrid>(costmap_topic_, 1, &RPRoadmapFilter::costMapCallback, this);
+        hppits_sub_ = nh_.subscribe<nav_msgs::OccupancyGrid>(costmap_topic_, 1, &RPRoadmapFilter::hppitsMapCallback, this);
 
         // publications of this node (for visualisation purposes), waypoints and connectivity information (edges)
         waypoints_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("viz/waypoints", 10, true);
@@ -66,6 +68,12 @@ namespace KCL_rosplan {
     void RPRoadmapFilter::costMapCallback(const nav_msgs::OccupancyGridConstPtr& msg) {
         cost_map_ = *msg;
         costmap_received_ = true;
+    }
+
+
+    void RPRoadmapFilter::hppitsMapCallback(const nav_msgs::OccupancyGridConstPtr& msg) {
+        hppits_map_ = *msg;
+        hppitsmap_received_ = true;
     }
 
     /*------------------------------*/
@@ -349,6 +357,17 @@ namespace KCL_rosplan {
         std::stringstream ss;
         ss << wp_namespace_output_ << "/" << wp_id;
         nh_.setParam(ss.str(), pose_as_array);
+
+        ros::Rate loop_rate(10);
+        while (not hppitsmap_received_ and ros::ok()) {
+            loop_rate.sleep();
+            ROS_INFO("KCL: (%s) Waiting for hppits map...", ros::this_node::getName().c_str());
+        }
+        // Get preference value
+        int cell_x = (int) (waypoint.pose.position.x/hppits_map_.info.resolution);
+        int cell_y = (int) (waypoint.pose.position.y/hppits_map_.info.resolution);
+        double value = hppits_map_.data[cell_x + cell_y*hppits_map_.info.width];
+        nh_.setParam(wp_namespace_output_ + "_pref/" + wp_id, value);
     }
 
     void RPRoadmapFilter::pubWPGraph() {
