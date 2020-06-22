2de6e87ffaf39e5d9d28e1536e5a975a8e3690a8
Gerard Canal <grar.knal@gmail.com>
2020-06-22 18:08:34 +0100

    Added anytime version and results printing

--- rosplan_waypoint_sampling/src/main_executor.py
+++ rosplan_waypoint_sampling/src/main_executor.py
@@ -12,11 +12,11 @@ from rosplan_knowledge_msgs.srv import *
 from rosplan_interface_mapping.srv import CreatePRM
 from rosplan_dispatch_msgs.srv import DispatchService, DispatchServiceResponse, PlanningService, PlanningServiceResponse
 from diagnostic_msgs.msg import KeyValue
+import threading
 
 ############
 # THE REST #
 ############
-DISPATCH = True
 
 # get path of pkg
 rospack = rospkg.RosPack()
@@ -33,17 +33,14 @@ data_path = rospy.get_param('~data_path', "")
 initial_state = rospy.get_param('~initial_state', "param_not_set")
 results_path = rospy.get_param('~results_path', "results.csv")
 planner_command = rospy.get_param('~planner_command', "")
+time_limit = rospy.get_param('~total_time_limit', 300)  # in seconds?
+planning_time_limit = rospy.get_param('~planning_time_limit', 10.0)  # in seconds?
 
 # wait for services
 rospy.wait_for_service('/rosplan_roadmap_server/create_prm')
 rospy.wait_for_service('/rosplan_problem_interface/problem_generation_server')
 rospy.wait_for_service('/rosplan_planner_interface/planning_server_params')
 rospy.wait_for_service('/rosplan_knowledge_base/state/propositions')
-
-if DISPATCH:
-    rospy.wait_for_service('/rosplan_plan_dispatcher/dispatch_plan')
-    rospy.wait_for_service('/rosplan_parsing_interface/parse_plan')
-
 if approach == 0:
     rospy.wait_for_service('/waypoint_sampler/sample_waypoints')
 
@@ -59,36 +56,21 @@ def plan_callback(msg):
 
 sub_once = rospy.Subscriber("/rosplan_planner_interface/planner_output", String, plan_callback)
 
+
+class Results:
+    def __init__(self, plan=[], time=0, plan_duration=0, pref_cost=float('inf')):
+        self.plan = plan
+        self.time = time
+        self.plan_duration = plan_duration
+        self.pref_cost = pref_cost
+
+
 def make_prm(size):
     # generate dense PRM
     rospy.loginfo("KCL: (%s) Creating PRM of size %i" % (rospy.get_name(), size))
     prm = rospy.ServiceProxy('/rosplan_roadmap_server/create_prm', CreatePRM)        
     if not prm(size,0.8,1.6,2.0,50,200000):
         rospy.logerr("KCL: (%s) No PRM was made" % rospy.get_name())
-
-
-def execute_plan():
-
-    rospy.loginfo("KCL: (%s) Calling plan parser" % rospy.get_name())
-    pp = rospy.ServiceProxy('/rosplan_parsing_interface/parse_plan', Empty)
-    if not pp():
-        rospy.logerr("KCL: (%s) The plan was not parsed!" % rospy.get_name())
-        return
-
-    rospy.loginfo("KCL: (%s) Calling plan execution" % rospy.get_name())
-    pd = rospy.ServiceProxy('/rosplan_plan_dispatcher/dispatch_plan', DispatchService)
-    pd_response = pd()
-
-    if not pd_response:
-        rospy.logerr("KCL: (%s) No response from the dispatch server." % rospy.get_name())
-        return False
-    if not pd_response.goal_achieved:
-        rospy.loginfo("KCL: (%s) The execution was not successful." % rospy.get_name())
-        return False
-    else:
-        rospy.loginfo("KCL: (%s) Plan was executed." % rospy.get_name())
-        return True
-
 
 def wait_for_sensing():
 
@@ -156,7 +138,7 @@ def generate_problem_and_plan():
         return True
 
 def plan_cost():
-    global planning_time, plan
+    global planning_time, plan, start_time
 
     action_list = plan.splitlines()
 
@@ -204,10 +186,14 @@ def plan_cost():
     incurred_cost = time_at_wp * current_wp_cost
     total_cost = total_cost + incurred_cost
     print "increasing cost by: " + str(time_at_wp) + " * " + str(current_wp_cost) + " = " +  str(incurred_cost) 
+    return Results(plan, rospy.Time.now()-start_time, plan_duration, total_distance, total_cost)
 
+def write_plan(results_first, results_best):
     try:
         f = open(results_path, "a")
-        f.write(str(approach)+","+os.path.basename(initial_state)+","+str(plan_duration)+","+str(total_distance)+","+str(planning_time)+","+str(total_cost)+"\n")
+        f.write(str(approach)+","+os.path.basename(initial_state)+","+str(results_first.time) + ',' +
+                str(results_best.time) + ',' + str(results_first.plan_duration) + ',' + str(results_best.plan_duration)
+                + ',' + str(results_first.pref_cost) + ',' + str(results_best.pref_cost)+"\n")
     except:
         rospy.logerr("KCL: (%s) Error writing to results file." % rospy.get_name())     
     finally:
@@ -222,6 +208,67 @@ def plan_failed():
     finally:
         f.close()
 
+
+best_vals = Results() #([], 0, 0, 0, float('inf'))  # (plan, plan_duration, total_distance, cost)
+first_vals = Results()
+lock = threading.Lock()
+start_time = -1
+finished = False
+
+def main_sampling_loop():
+    global best_vals, first_vals, max_sample_size,plan_recieved, lock
+
+    make_prm(max_prm_size)
+
+    resamples = 0
+    sample_count = 12
+    goal_achieved = False
+    if approach == 2:
+        max_sample_size = sample_count + 1
+
+    while sample_count < max_sample_size or (approach == 0):
+
+        rospy.loginfo("KCL: (%s) Sampling %i waypoints" % (rospy.get_name(), sample_count))
+        smp = rospy.ServiceProxy('/waypoint_sampler/sample_waypoints', SetInt)
+        if not smp(sample_count):
+            rospy.logerr("KCL: (%s) No sample was made" % rospy.get_name())
+
+        wait_for_sensing()
+
+        plan = ""
+        plan_recieved = False
+        plan_found = generate_problem_and_plan()
+
+        if not plan_found:
+            if planning_time >= planning_time_limit and approach == 0:
+                if resamples > 4:
+                    # break
+                    pass
+                # timeout, decrease sample size
+                sample_count -= 4
+                if sample_count < 8:
+                    sample_count = 8
+                resamples += 1
+            else:
+                sample_count += 2
+        else:
+            # Wait for plan
+            while not rospy.is_shutdown() and not plan_recieved:
+                rospy.loginfo("KCL: (%s) Plan not received, waiting..." % rospy.get_name())
+                rospy.sleep(0.5)
+            res = plan_cost()
+            lock.acquire()
+            if res.cost < best_vals.cost:
+                best_vals = res
+            lock.release()
+            if not first_vals.plan:  # if no plan yet
+                first_vals = res
+            if finished:
+                return None  # End thread
+            # break
+    return None
+
+
 ### EXPERIMENT ###
 try:
     rospy.sleep(1)
@@ -234,7 +281,7 @@ try:
 
             plan_found = generate_problem_and_plan()
             if not plan_found:
-                if planning_time>=10.00:
+                if planning_time>=planning_time_limit:
                     # timeout, stop here
                     sample_count = max_prm_size
                 else:
@@ -245,56 +292,40 @@ try:
                 while not rospy.is_shutdown() and not plan_recieved:
                     rospy.loginfo("KCL: (%s) Plan not received, waiting..." % rospy.get_name())
                     rospy.sleep(0.5)
-                plan_cost()
+                results = plan_cost()
+                write_plan(results, results)  # As we stop in the first solution, best results are first results.
                 break
         if not plan_found:
             plan_failed()
 
     ### SAMPLING APPROACH ###
-    resamples = 0
     if approach == 0 or approach == 2:
+        start_time = rospy.Time.now()
+        t = threading.Thread(target=main_sampling_loop)
+        t.start()
 
-        make_prm(max_prm_size)
-
-        sample_count = 12
-        goal_achieved = False
-        if approach==2:
-            max_sample_size = sample_count + 1
-
-        while sample_count < max_sample_size:
-
-            rospy.loginfo("KCL: (%s) Sampling %i waypoints" % (rospy.get_name(), sample_count))
-            smp = rospy.ServiceProxy('/waypoint_sampler/sample_waypoints', SetInt)        
-            if not smp(sample_count):
-                rospy.logerr("KCL: (%s) No sample was made" % rospy.get_name())
-
-            wait_for_sensing()
-
-            plan = ""
-            plan_recieved = False
-            plan_found = generate_problem_and_plan()
-
-            if not plan_found:
-                if planning_time>=10.00 and approach==0:
-                    if resamples>4:
-                        break
-                    # timeout, decrease sample size
-                    sample_count -= 4
-                    if sample_count < 8:
-                        sample_count = 8
-                    resamples += 1
-                else:
-                    sample_count += 2
-            else:
-                # Wait for plan
-                while not rospy.is_shutdown() and not plan_recieved:
-                    rospy.loginfo("KCL: (%s) Plan not received, waiting..." % rospy.get_name())
-                    rospy.sleep(0.5)
-                plan_cost()
-                execute_plan()
-                break
-        if not plan_found:
+        if (approach == 0):
+            t.join(float(time_limit))  # it will timeout after time_limit seconds
+            finished = True
+        else:
+            t.join()
+        lock.acquire()  # Lock thread in case it's still running so it doesn't update variables after timeout. Thread will be killed after the program finishes
+        print '\n\n\n'
+        if not best_vals.plan:
             plan_failed()
+        else:
+            rospy.loginfo("KCL: (%s) After %f seconds: duration %f, distance %f, cost %f" % (rospy.get_name(), time_limit, best_vals[1], best_vals[2], best_vals[3]))
+            write_plan(first_vals, best_vals) # plan_duration, total_distance, total_cost
+        rospy.loginfo("KCL: (%s) Total time: %f minutes" % (rospy.get_name(), (rospy.Time.now() - start_time).to_sec() / 60.0))
+        rospy.loginfo("KCL: (%s) Time limit: %f minutes" % (rospy.get_name(), (time_limit/60.0)))
+        print '\n\n\n'
+
+        # Check if thread still runnning
+        lock.release()
+        if t.isAlive():
+            rospy.loginfo("KCL: (%s) Thread still alive after timeout, waiting for it to end" % rospy.get_name())
+            t.join()  # Wait for thread to finish nicely
+
 
 except rospy.ServiceException, e:
     rospy.logerr("KCL: (%s) Service call failed: %s" % (rospy.get_name(), e))
